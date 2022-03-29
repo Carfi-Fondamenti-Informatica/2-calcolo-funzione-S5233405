85386138c0fd06e967e3ee3f6ac34030dd939acb
S5233405 <101658438+S5233405@users.noreply.github.com>
2022-03-29 20:32:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main() {
               int  z=a*x - b*y;
               cout << z;
           }else if(x>=0 and y<=0){
-              int z=a*x*x - b*y ;
+              int z=a*(x*x) - b*y ;
               cout << z;
           }else{
-              int z = a*x + b*y*y;
+              int z = a*x + b*(y*y);
               cout << z;
           }
             
