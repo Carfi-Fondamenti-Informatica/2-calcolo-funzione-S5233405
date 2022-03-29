0d7efe5f028cd9ea8cf60daa3e1d538bff68aa77
S5233405 <101658438+S5233405@users.noreply.github.com>
2022-03-29 20:16:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main() { int a=0, b=0, x=0,y=0;
+  cin >> a >> b >> x >> y;
+          if (x<0 and y>0){
+              int  z=a*x - b*y;
+              cout << z;
+          }else if(x>=0 and y<=0){
+              int z=a*x*x - b*y ;
+              cout << z;
+          }else{
+              int z = a*x + b*y*y;
+              cout << z;
+          }
+            
+ return 0;
 }
